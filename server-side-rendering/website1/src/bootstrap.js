e8d6eecc2dc7190db6ff2879a1ac7387808ed6b3
bixia <bixia@ebay.com>
2022-01-12 15:23:26 +0800

    update bootstrap

--- server-side-rendering/website1/src/bootstrap.js
+++ server-side-rendering/website1/src/bootstrap.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import App from "./components/App";
 
-const render = (App) => {
+import { loadableReady } from '@loadable/component'
+
+loadableReady(() => {
   const root = document.getElementById("root");
 
   ReactDOM.hydrate(
@@ -12,15 +14,4 @@ const render = (App) => {
     </AppContainer>,
     root
   );
-};
-
-render(App);
-
-if (module.hot && process.env.NODE_ENV === "development") {
-  module.hot.accept("./components/App", () => {
-    // eslint-disable-next-line
-    const App = require("./components/App").default;
-
-    render(App);
-  });
-}
+})
