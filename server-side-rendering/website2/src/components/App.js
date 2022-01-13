b3fc4e93f1a887ba731aaf79a21763de7fb0cb37
bixia <bixia@ebay.com>
2022-01-13 16:11:10 +0800

    add effect hooks in Somecomponent

--- server-side-rendering/website2/src/components/App.js
+++ server-side-rendering/website2/src/components/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./SomeComponent.js";
 
-export default (props) => (
-  <div>
-    <Header {...props} />
-  </div>
-);
+const BROWSER = typeof window !== 'undefined'
+
+export default (props) => {
+  const [fullUI, setFullUI] = useState(!!Object.keys(props).length);
+  const [renderData, setRenderData] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!fullUI) {
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+        const result = await {
+          name: 'website2 generated data which can be also used for SSR'
+        }
+        setRenderData(result)
+        setFullUI(true)
+      }
+    }
+
+    fetchData()
+
+  })
+
+  return (
+    <div>
+      {
+        fullUI ?
+          <Header {...renderData} />
+          :
+          <>loading......</>
+      }
+
+      this is the website2 and we will append footer component later...
+    </div>
+  )
+};
 
 export async function getServerSideProps() {
   return {
